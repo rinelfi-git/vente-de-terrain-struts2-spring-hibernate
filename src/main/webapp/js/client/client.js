8cf8c1becc7978650beba51b627dba9400e2e1d1
rinelfi <elierijaniaina@gmail.com>
2022-03-05 12:59:12 +0300

    update: ajouter des fins d'instructions

--- src/main/webapp/js/client/client.js
+++ src/main/webapp/js/client/client.js
@@ -1,15 +1,15 @@
-let paginationCurrentPage = 1
-let paginationElementPerPage = 12
-let pageLength = 1
-let identity = -1
+let paginationCurrentPage = 1;
+let paginationElementPerPage = 12;
+let pageLength = 1;
+let identity = -1;
 
 function udpateClientProfileImage(id) {
-    identity = id
-    $('#update-profile-image').modal('show')
+    identity = id;
+    $('#update-profile-image').modal('show');
 }
 
 function getDataFromService() {
-    var keyword = $('[name=keyword]').val()
+    var keyword = $('[name=keyword]').val();
     $.ajax({
         method: 'post',
         url: baseUrl('client/pagination.action'),
@@ -25,25 +25,25 @@ function getDataFromService() {
         },
         dataType: 'json',
         success: function (response) {
-            clientsHtml = ''
-            for (client of response) {
-                clientsHtml += clientCardTemplate(client.id, client.nom, client.prenom, client.cin, client.adresse, client.telephones, client.photo)
+            clientsHtml = '';
+            for (const client of response) {
+                clientsHtml += clientCardTemplate(client.id, client.nom, client.prenom, client.cin, client.adresse, client.telephones, client.photo);
             }
-            $('#client-card-view').html(clientsHtml)
-            getPagesList()
+            $('#client-card-view').html(clientsHtml);
+            getPagesList();
         }
-    })
-    return false
+    });
+    return false;
 }
 
 function navigatePaginationTo(target) {
-    var min = Math.min(target, pageLength), max = Math.max(1, target)
-    paginationCurrentPage = min == pageLength ? min : max
-    getDataFromService()
+    var min = Math.min(target, pageLength), max = Math.max(1, target);
+    paginationCurrentPage = min == pageLength ? min : max;
+    getDataFromService();
 }
 
 function getPagesList() {
-    var keyword = $('[name=keyword]').val()
+    var keyword = $('[name=keyword]').val();
     $.ajax({
         method: 'post',
         url: baseUrl('client/records.action'),
@@ -56,39 +56,34 @@ function getPagesList() {
         },
         dataType: 'json',
         success: function (response) {
-            pageLength = Math.ceil(response / paginationElementPerPage)
-            $('#client-pagination').html(paginationTemplate(1, pageLength, paginationCurrentPage))
+            pageLength = Math.ceil(response / paginationElementPerPage);
+            $('#client-pagination').html(paginationTemplate(1, pageLength, paginationCurrentPage));
         }
-    })
+    });
 }
 
 function checkInputCin(element) {
-    const insertionNombre = !isNaN(parseInt(element.value.slice(-1)))
+    const insertionNombre = !isNaN(parseInt(element.value.slice(-1)));
     if (insertionNombre) {
         const cassures = [3, 7, 11];
         const selectionStart = element.selectionStart;
-        let correction = element.value.replaceAll('-', '')
+        let correction = element.value.replaceAll('-', '');
         cassures.forEach(function (cassure) {
             if (correction.length > cassure)
-                correction = correction.slice(0, cassure) + '-' + correction.slice(cassure, correction.length)
-        })
-        element.value = correction
-        if(element.value.length -1 !== selectionStart || element.value.slice(-2, -1) !== '-') element.setSelectionRange(selectionStart, selectionStart)
+                correction = correction.slice(0, cassure) + '-' + correction.slice(cassure, correction.length);
+        });
+        element.value = correction;
+        if(element.value.length -1 !== selectionStart || element.value.slice(-2, -1) !== '-') element.setSelectionRange(selectionStart, selectionStart);
     } else
-        element.value = element.value.slice(0, -1)
+        element.value = element.value.slice(0, -1);
 }
 
 function validateStepForm(step, scope, button) {
-    $('.is-invalid').removeClass('is-invalid')
-    $('.form-text').css({display: 'none'})
-    const scopes = [
-        `${scope}-personal-form`,
-        `${scope}-phones-form`,
-        `${scope}-address-form`
-    ]
+    $('.is-invalid').removeClass('is-invalid');
+    $('.form-text').css({display: 'none'});
 
-    let validators = []
-    let validatorValid = null
+    let validators = [];
+    let validatorValid = null;
 
     switch (parseInt(step)) {
         case 1:
@@ -101,7 +96,7 @@ function validateStepForm(step, scope, button) {
                 if (!validator.isValid()) {
                     return validator;
                 }
-            })
+            });
             if (typeof validatorValid === 'undefined') {
                 loadStepForm(2, scope);
             } else {
@@ -110,7 +105,7 @@ function validateStepForm(step, scope, button) {
                         validator.field.classList.add('is-invalid');
                         validator.putError(`${validator.field.getAttribute('id')}-error`);
                     }
-                })
+                });
             }
             break;
         case 2:
@@ -118,24 +113,24 @@ function validateStepForm(step, scope, button) {
             
             const phones = document.querySelectorAll(`.${scope}-phone-input`);
             for (let i = 0; i < phones.length; i++) {
-                validators.push($formValidation(`${scope}-phone${i}`))
+                validators.push($formValidation(`${scope}-phone${i}`));
             }
             
             validatorValid = validators.find(function (validator) {
                 if (!validator.isValid()) {
                     return validator;
                 }
-            })
+            });
 
             if (typeof validatorValid === 'undefined') {
-                loadStepForm(3, scope)
+                loadStepForm(3, scope);
             } else {
                 validators.forEach(function (validator) {
                     if (!validator.isValid()) {
                         validator.field.classList.add('is-invalid');
                         validator.putError(`${validator.field.getAttribute('id')}-error`);
                     }
-                })
+                });
             }
             break;
         case 3:
@@ -147,11 +142,11 @@ function validateStepForm(step, scope, button) {
 function recheckStepForm(step, scope) {
     switch (parseInt(step)) {
         case 2:
-            loadStepForm(1, scope)
-            break
+            loadStepForm(1, scope);
+            break;
         case 3:
-            loadStepForm(2, scope)
-            break
+            loadStepForm(2, scope);
+            break;
     }
     return false;
 }
@@ -172,23 +167,23 @@ function loadStepForm(step, scope) {
             document.getElementById(`${scope}-previous`).style.display = 'none';
             document.getElementById(`${scope}-next`).style.display = 'block';
             document.getElementById(`${scope}-next`).dataset.target = 1;
-            document.getElementById(`${scope}-save`).style.display = 'none'
-            break
+            document.getElementById(`${scope}-save`).style.display = 'none';
+            break;
         case 2:
-            document.getElementById(`${scope}-cancel`).style.display = 'none'
-            document.getElementById(`${scope}-previous`).style.display = 'block'
-            document.getElementById(`${scope}-previous`).dataset.target = 2
-            document.getElementById(`${scope}-next`).style.display = 'block'
-            document.getElementById(`${scope}-next`).dataset.target = 2
-            document.getElementById(`${scope}-save`).style.display = 'none'
-            break
+            document.getElementById(`${scope}-cancel`).style.display = 'none';
+            document.getElementById(`${scope}-previous`).style.display = 'block';
+            document.getElementById(`${scope}-previous`).dataset.target = 2;
+            document.getElementById(`${scope}-next`).style.display = 'block';
+            document.getElementById(`${scope}-next`).dataset.target = 2;
+            document.getElementById(`${scope}-save`).style.display = 'none';
+            break;
         case 3:
-            document.getElementById(`${scope}-cancel`).style.display = 'none'
-            document.getElementById(`${scope}-previous`).style.display = 'block'
-            document.getElementById(`${scope}-previous`).dataset.target = 3
-            document.getElementById(`${scope}-next`).style.display = 'none'
-            document.getElementById(`${scope}-save`).style.display = 'block'
-            break
+            document.getElementById(`${scope}-cancel`).style.display = 'none';
+            document.getElementById(`${scope}-previous`).style.display = 'block';
+            document.getElementById(`${scope}-previous`).dataset.target = 3;
+            document.getElementById(`${scope}-next`).style.display = 'none';
+            document.getElementById(`${scope}-save`).style.display = 'block';
+            break;
     }
 }
 
@@ -205,7 +200,7 @@ $(document).ready(function () {
         icon.css({
             position: 'absolute',
             top: ($(e.currentTarget).height() / 2 - icon.height() / 2) + 'px',
-            left: (7 + $(e.currentTarget).width() / 2 - icon.width() / 2) + 'px',
+            left: (7 + $(e.currentTarget).width() / 2 - icon.width() / 2) + 'px'
         });
         icon.fadeIn(animationDuration);
         background.addClass('overlay-active');
@@ -215,8 +210,8 @@ $(document).ready(function () {
             icon.stop();
             icon.fadeOut(animationDuration);
             background.removeClass('overlay-active');
-        })
-    })
+        });
+    });
 
     $(document).on('mouseover', '.custom-class-image-edit-overlay-foreground', e => {
         var icon = $(e.currentTarget).prev('.fa');
@@ -227,7 +222,7 @@ $(document).ready(function () {
         icon.css({
             position: 'absolute',
             top: ($(e.currentTarget).height() / 2 - icon.height() / 2) + 'px',
-            left: (7 + $(e.currentTarget).width() / 2 - icon.width() / 2) + 'px',
+            left: (7 + $(e.currentTarget).width() / 2 - icon.width() / 2) + 'px'
         });
         icon.fadeIn(animationDuration);
         background.addClass('overlay-active');
@@ -237,7 +232,7 @@ $(document).ready(function () {
             icon.stop();
             icon.fadeOut(animationDuration);
             background.removeClass('overlay-active');
-        })
-    })
-    getDataFromService()
-})
+        });
+    });
+    getDataFromService();
+});
